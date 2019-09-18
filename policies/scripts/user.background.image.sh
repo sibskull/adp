f05d2c813a4a22ad3236072a6f0fd83801a91edd
Andrey Cherepanov <cas@altlinux.org>
2019-09-18 12:11:28 +0300

    Add example policy template

---
+++ policies/scripts/user.background.image.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. adp-functions
+
+# Set background image for user
+IMAGE="$1"
+LOCATION="$2"
+
+# Copy image from location
+IMAGE_PATH="$(xdg-user-dir PICTURES)/.$IMAGE"
+adp_copy_file "$LOCATION" "$IMAGE" "$IMAGE_PATH"
+
+gsettings set org.mate.background picture-filename "$IMAGE_PATH"
