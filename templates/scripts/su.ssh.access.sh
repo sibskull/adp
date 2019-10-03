c6b1abc01b49e210ab8830c8df71e2191ba5eec4
Leontiy Volodin <lvol@altlinux.org>
2019-10-03 11:18:36 +0300

    Finished ssh.access template

--- templates/scripts/su.ssh.access.sh
+++ templates/scripts/su.ssh.access.sh
@@ -2,11 +2,14 @@
 
 . adp-functions
 
-USER="$1"
-GROUP="$2"
+SWITCH="$1" # true (allow access) / false (deny access)
+USER="$2"
+GROUP="$3"
 
 conf="/etc/openssh/sshd_config"
 
+if [ "$SWITCH" = true ]; then
+{
 if (grep -ru "^#AllowUsers" $conf); then
     sed -i "s|^#AllowUsers.*|AllowUsers $USER|" $conf
 elif (grep -ru "^AllowUsers" $conf); then
@@ -22,3 +25,25 @@ elif (grep -ru "^AllowGroups" $conf); then
 else
     echo "AllowGroups $GROUP" >> $conf
 fi
+}
+else
+{
+if (grep -ru "^#DenyUsers" $conf); then
+    sed -i "s|^#DenyUsers.*|DenyUsers $USER|" $conf
+elif (grep -ru "^DenyUsers" $conf); then
+    sed -i "s|^DenyUsers.*|DenyUsers $USER|" $conf
+else
+    echo "DenyUsers $USER" >> $conf
+fi
+
+if (grep -ru "^#DenyGroups" $conf); then
+    sed -i "s|^#DenyGroups.*|AllowGroups $GROUP|" $conf
+elif (grep -ru "^DenyGroups" $conf); then
+    sed -i "s|^DenyGroups.*|AllowGroups $GROUP|" $conf
+else
+    echo "DenyGroups $GROUP" >> $conf
+fi
+}
+fi
+
+systemctl restart sshd
