bbac2f4edeafa8da65b18264569f12b77af62342
Leontiy Volodin <lvol@altlinux.org>
2019-10-04 17:46:12 +0300

    Added '-y' to package.remove template

--- templates/scripts/su.package.remove.sh
+++ templates/scripts/su.package.remove.sh
@@ -6,7 +6,7 @@ CLEAN_SETTINGS="$1" # clean the settings of removed packages
 PACKAGES="$2" # list of packages for remove
 
 if [ "$CLEAN_SETTINGS" = true ]; then
-apt-get remove "$PACKAGES" --purge
+apt-get remove "$PACKAGES" --purge -y
 else
-apt-get remove "$PACKAGES"
+apt-get remove "$PACKAGES" -y
 fi
