e949c0b23ba63742def2dd6dcdde13c93526ef7f
Leontiy Volodin <lvol@altlinux.org>
2019-10-08 13:15:23 +0300

    Fixed user.screensaver.lock.sh

--- templates/scripts/user.screensaver.lock.sh
+++ templates/scripts/user.screensaver.lock.sh
@@ -3,5 +3,5 @@
 # On/off lock on activation the screensaver
 STATE="$1"
 
-[ "$STATE" == "true" ] && gsettings set org.mate.screensaver idle-activation-enabled true
+[ "$STATE" = "true" ] && gsettings set org.mate.screensaver idle-activation-enabled true
 gsettings set org.mate.screensaver lock-enabled "$STATE"
