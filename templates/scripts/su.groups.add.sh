c4d9bf4a22a8ebfe0434cf6a6bfa8a7ff6b01fc7
Leontiy Volodin <lvol@altlinux.org>
2019-10-10 17:02:04 +0300

    Small corrections of names of variables in templates

--- templates/scripts/su.groups.add.sh
+++ templates/scripts/su.groups.add.sh
@@ -3,12 +3,12 @@
 . adp-functions
 
 GROUP="$1"
-PASSWD="$2"
+PASSWORD="$2"
 
 #You need to pass the encrypted password for the utility. If you pass the password in the clear, then nothing will not work.
 #To generate a password hash using crypt and python3.
-PASSWD_ENCRYPT=$(python3 -c "import random,string,crypt;
+PASSWORD_ENCRYPT=$(python3 -c "import random,string,crypt;
 randomsalt = ''.join(random.sample(string.ascii_letters,8));
-print crypt.crypt('$PASSWD', '\$6\$%s\$' % randomsalt)")
+print crypt.crypt('$PASSWORD', '\$6\$%s\$' % randomsalt)")
 
-groupadd -p "$PASSWD_ENCRYPT" "$GROUP"
+groupadd -p "$PASSWORD_ENCRYPT" "$GROUP"
