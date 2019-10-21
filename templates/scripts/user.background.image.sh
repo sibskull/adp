a2fc56efeee0c707931af8de0d6c13911e399232
Evgeniy Korneechev <ekorneechev@altlinux.org>
2019-10-21 17:05:04 +0300

    Updated user.background.image.sh

--- templates/scripts/user.background.image.sh
+++ templates/scripts/user.background.image.sh
@@ -6,8 +6,7 @@
 LOCATION="$1"
 
 # Copy image from location
-IMAGE="$(basename "$LOCATION")"
-IMAGE_PATH="$(xdg-user-dir PICTURES)/.$IMAGE"
+IMAGE_PATH="$(xdg-user-dir PICTURES)/.background.${LOCATION##*.}"
 adp_copy_file "$LOCATION" "$IMAGE_PATH"
 
 gsettings set org.mate.background picture-filename "$IMAGE_PATH"
